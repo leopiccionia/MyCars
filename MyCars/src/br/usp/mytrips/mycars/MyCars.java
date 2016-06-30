9acd9a577357dbf1add75b3cedf6677c85374345
leo <leo@debian-leo>
2016-06-30 16:05:39 -0300

    Tentative CriteriaQuery()?

--- MyCars/src/br/usp/mytrips/mycars/MyCars.java
+++ MyCars/src/br/usp/mytrips/mycars/MyCars.java
@@ -1,11 +1,13 @@
 package br.usp.mytrips.mycars;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.persistence.*;
+import javax.persistence.criteria.*;
 
 @Stateless
 @WebService(portName="MyCarsPort", serviceName="MyCarsService", targetNamespace="http://localhost/MyCars/wsdl", endpointInterface="MyCarsWS")
@@ -26,8 +28,19 @@ public class MyCars implements MyCarsWS{
 
 	@WebMethod
 	public List<Carro> local(String cidade) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("carros");
+		EntityManager manager= factory.createEntityManager();	
+		CriteriaBuilder cb = manager.getCriteriaBuilder();
+		CriteriaQuery<Carro> cq = cb.createQuery(Carro.class);
+		Root<Carro> root = cq.from(Carro.class);
+		ParameterExpression<String> arg = cb.parameter(String.class, "cidade");
+		cq.select(root).where(cb.and(cb.equal(root.get("cidade"), arg)));
+		TypedQuery<Carro> tq = manager.createQuery(cq);
+		tq.setParameter(arg, cidade);
+		List<Carro> lista = tq.getResultList();		
+		manager.close();
+		factory.close();
+		return lista;
 	}
 	
 }
